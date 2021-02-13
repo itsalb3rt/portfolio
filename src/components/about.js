2ecc7d83bfb2ce70f0c8216d7f311ee116bd6396
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2021-02-13 13:53:19 -0400

    feat
    * Added rounded about photo

--- src/components/about.js
+++ src/components/about.js
@@ -22,7 +22,7 @@ const About = () => {
             </p>
           </div>
           <div className="image-wrapper">
-            <img src={data.aboutImage} alt="about"></img>
+            <img className="about-image" src={data.aboutImage} alt="about"></img>
           </div>
         </div>
       </div>

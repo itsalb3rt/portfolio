aed94127174cfa5ada4ea9694630cfcab23befe7
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2021-02-22 20:00:20 -0400

    feat
    * Removed background linear color from cards

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -4,12 +4,7 @@ const Card = ({ heading, paragraph, imgUrl, projectLink }) => {
   return (
     <div
       className="card"
-      style={{
-        backgroundImage:
-          "linear-gradient(to bottom, rgba(245, 246, 252, 0), rgba(0, 0, 0, 0.2)),url(" +
-          imgUrl +
-          ")",
-      }}
+      style={{ backgroundImage: `url("${imgUrl}")` }}
     >
       <div className="content">
         <h1 className="header">{heading}</h1>

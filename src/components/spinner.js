4b6fdd5f090561cb3168abc5c75fc2d50c1a0c39
Albert Hidalgo <alhidalgodev@gmail.com>
2023-05-25 17:45:35 -0400

    feat
    * Added funcionality to the contact form

---
+++ src/components/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
+
+export default Spinner;
